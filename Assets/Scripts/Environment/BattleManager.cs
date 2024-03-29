f6a46d5795e2fb5875cafb1988ea82eaac21bb59
Tyler Lynch <tlynch@cohsbusiness.edu>
2024-03-29 10:23:33 -0700

    Worked on selection system

--- Assets/Scripts/Environment/BattleManager.cs
+++ Assets/Scripts/Environment/BattleManager.cs
@@ -10,32 +10,31 @@ public class BattleManager : MonoBehaviour
     /// AM = Attack Multiplier, the amount of damage a character's attack is mutiplied by
     /// BA = Bonus Attack, additional damage that isn't affected by the damage calculation
     /// DD = Defense Divider, the value an attack's total damage is divided by
-    /// BD = Bonus Defense, additional defense that isn't affected by the defense divider
+    /// BD = Bonus Defense, additional defense that isn't affected by the defense divider 
+    [SerializeField] string characterName;
+    public Camera BattleCamera;
     private Party party; 
     public EnemyManager inBattle;
     public Health health;
     public Transform chosenCharacter;
     public Transform chosenEnemy;
 
+    private void Start()
+    {
+        BattleCamera = Camera.main;
+    }
     void Awake()
     {
 
     }
-    void chooseCharacter()
-    {
-        if (chosenCharacter.name == "Lateef")
-        {
-
-        }
-    }
-
-    private void Start()
+    void OnButtonDown()
     {
 
     }
+
 
     private void Update()
     {
-    
+
     }
 }
